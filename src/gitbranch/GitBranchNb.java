3c3f213f6712eef58eddc32d45fcc2ce664e1ed0
lajko.gergo <lajko.gergo@DESKTOP-UQIGT8P.szaleziakk.local>
2024-09-30 08:52:09 +0200

    static void fv()

--- src/gitbranch/GitBranchNb.java
+++ src/gitbranch/GitBranchNb.java
@@ -5,7 +5,11 @@ package gitbranch;
 public class GitBranchNb {
 
     public static void main(String[] args) {
-  
+        fv();
+    }
+
+    private static void fv() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
